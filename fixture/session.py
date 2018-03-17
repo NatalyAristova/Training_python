7a4b4611bda7002082b08a1786714ac325f53d78
Nataly Aristova <AristovaNatalia87@yandex.ru>
2018-03-18 01:37:47 +0300

    Задание 8.2.

--- fixture/session.py
+++ fixture/session.py
@@ -14,18 +14,18 @@ class SessionHelper:
                 self.logout()
         self.login(username, password)
 
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+
     def ensure_logout(self):
         wd = self.app.wd
         if self.is_logged_in():
             self.logout()
 
-    def is_logged_in_as(self, username):
-        wd = self.app.wd
-        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
-
     def is_logged_in(self):
         wd = self.app.wd
-        return len(wd.find_elements_by_link_text("Logout").click()) > 0
+        return len(wd.find_elements_by_link_text("Logout")) > 0
 
     def logout(self):
         wd = self.app.wd
