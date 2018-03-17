0287af66106e1e5cb947feb6fde5484c70eb1587
Nataly Aristova <AristovaNatalia87@yandex.ru>
2018-03-17 23:12:06 +0300

    test_modify_contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -9,6 +9,14 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
 
+    def modify_first_contact(self, new_contact_data):
+        wd = self.app.wd
+        self.select_first_contact()
+        self.button_Edit()
+        self.fill_contact_form(new_contact_data)
+        self.button_Update()
+
+
     def edit_contact_firm(self, group_contact):
         wd = self.app.wd
         wd.find_element_by_name("firstname").click()
@@ -92,63 +100,38 @@ class ContactHelper:
         wd = self.app.wd
         # init contact creation
         wd.find_element_by_link_text("add new").click()
-        # fill group firm
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(group_contact.firstname)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(group_contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(group_contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(group_contact.nickname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(group_contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(group_contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(group_contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(group_contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(group_contact.home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(group_contact.mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(group_contact.work)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(group_contact.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(group_contact.email)
+        self.fill_contact_form(group_contact)
+        # enter
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.return_to_home_page()
+
+    def fill_contact_form(self, group_contact):
+        wd = self.app.wd
+        self.change_contact_value("firstname", group_contact.firstname)
+        #wd.find_element_by_name("theform").click()
+        self.change_contact_value("middlename", group_contact.middlename)
+        self.change_contact_value("lastname", group_contact.lastname)
+        self.change_contact_value("nickname", group_contact.nickname)
+        self.change_contact_value("title", group_contact.title)
+        self.change_contact_value("company", group_contact.company)
+        self.change_contact_value("address", group_contact.address)
+        self.change_contact_value("home", group_contact.home)
+        self.change_contact_value("mobile", group_contact.mobile)
+        self.change_contact_value("work", group_contact.work)
+        self.change_contact_value("fax", group_contact.fax)
+        self.change_contact_value("email", group_contact.email)
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[10]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[10]").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(group_contact.byear)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(group_contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(group_contact.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(group_contact.notes)
-        # enter
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.return_to_home_page()
+        self.change_contact_value("byear", group_contact.byear)
+        self.change_contact_value("address2", group_contact.address2)
+        self.change_contact_value("phone2", group_contact.phone2)
+        self.change_contact_value("notes", group_contact.notes)
+
+    def change_contact_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,7 @@
 from model.group_contact import Group_contact
 
 
-def test_add_contact(app):
+def test_modify_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Group_contact(firstname="Ivan", middlename="Sergeevich", lastname="Sergeev", nickname="Ssss", title="aaaa", company="ssss", address="rrrr", home="4", mobile="1111111",
                                      work="22222", fax="333", email="fff@ya.ru", byear="1990", address2="ffff", phone2="fff", notes="ggg"))
