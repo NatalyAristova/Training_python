03116cd1b0c4de17e9dfe8302878ef920ff26466
Nataly Aristova <AristovaNatalia87@yandex.ru>
2018-03-17 16:22:16 +0300

    test_modify_group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,6 +1,6 @@
 from model.group import Group
 
-def test_edit_first_group(app):
+def test_edit_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="edit1", header="edit1", footer=""))
+    app.group.edit_first_group(Group(name="New group", header="edit1", footer=""))
     app.session.logout()
