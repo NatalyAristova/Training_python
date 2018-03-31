bff91d83d67f018b37601dca711a06bd3d81bfbd
Nataly Aristova <AristovaNatalia87@yandex.ru>
2018-03-31 16:01:24 +0300

    5 лекция

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 from model.group_contact import Group_contact
+import re
 
 class ContactHelper:
 
@@ -10,14 +11,16 @@ class ContactHelper:
     def get_contact_list(self):
         if self.contact_cache is None:
             wd = self.app.wd
-            self.open_home_page()
+            self.app.open_home_page()
             self.contact_cache = []
             for element in wd.find_elements_by_css_selector("tr[name=entry]"):
                 cells = element.find_elements_by_tag_name("td")
                 id = element.find_element_by_name("selected[]").get_attribute("value")
                 lastname = cells[1].text
                 firstname = cells[2].text
-                self.contact_cache.append(Group_contact(id=id, lastname=lastname, firstname=firstname))
+                all_phones = cells[5].text
+                self.contact_cache.append(Group_contact(id=id, lastname=lastname, firstname=firstname,
+                                                        all_phones_from_home_page=all_phones))
         return list(self.contact_cache)
 
     def count(self):
@@ -27,6 +30,45 @@ class ContactHelper:
     def return_to_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row=wd.find_elements_by_name("entry")[index]
+        cell=row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home = wd.find_element_by_name("home").get_attribute("value")
+        work = wd.find_element_by_name("work").get_attribute("value")
+        mobile = wd.find_element_by_name("mobile").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        return Group_contact(firstname=firstname, lastname=lastname, id=id, home=home,
+                       mobile=mobile, work=work, phone2=phone2)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text=wd.find_element_by_id("content").text
+        home=re.search("H: (.*)", text).group(1)
+        work = re.search("W: (.*)", text).group(1)
+        mobile = re.search("M: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Group_contact(home=home,
+                             mobile=mobile, work=work, phone2=phone2)
+
 
     def modify_first_contact(self, index, new_contact_data):
         self.modify_contact_by_index(0)
--- model/group_contact.py
+++ model/group_contact.py
@@ -5,7 +5,7 @@ class Group_contact:
 
     def __init__(self,firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None,
                  address=None, home=None, mobile=None, work=None, fax=None, email=None, byear=None,
-                 address2=None, phone2=None, notes=None, id=None):
+                 address2=None, phone2=None, notes=None, all_phones_from_home_page=None, id=None):
         self.firstname=firstname
         self.middlename=middlename
         self.lastname=lastname
@@ -23,6 +23,7 @@ class Group_contact:
         self.phone2=phone2
         self.notes=notes
         self.id = id
+        self.all_phones_from_home_page=all_phones_from_home_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.lastname, self.firstname)
